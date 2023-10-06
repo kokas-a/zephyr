6ca2324cb94ada0b40865a02f4132e860aedf168
Nikolay Agishev <agishev@synopsys.com>
2023-10-06 19:38:30 +0300

    ARC: Tests with timeunits macros
    Applying wrappers for macro calls
    
    Signed-off-by: Nikolay Agishev <agishev@synopsys.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,120 @@
 
 #include <stdio.h>
 
+#include <zephyr/sys/time_units.h>
+#include <zephyr/types.h>
+
+#define param 33 / 33 + 1 -1
+
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD);
+	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	uint64_t tmp_val;
+
+	printk("k_ms_to_cyc_floor32(%d) = %u\r\n", param,   k_ms_to_cyc_floor32(param));
+
+	tmp_val =  k_ms_to_cyc_floor64(param);
+	printk("k_ms_to_cyc_floor64(%d) = %llu\r\n", param, tmp_val);
+	printk("k_ms_to_cyc_near32(%d)  = %u\r\n", param,   k_ms_to_cyc_near32(param));
+
+	tmp_val =  k_ms_to_cyc_near64(param);
+	printk("k_ms_to_cyc_near64(%d)  = %llu\r\n", param, tmp_val);
+	printk("k_ms_to_cyc_ceil32(%d)  = %u\r\n", param,   k_ms_to_cyc_floor32(param));
+
+	tmp_val =  k_ms_to_cyc_ceil64(param);
+	printk("k_ms_to_cyc_ceil64(%d)  = %llu\r\n", param, tmp_val);
+
+	printf("k_ms_to_ticks_floor32(%d) = %u\r\n", param,   k_ms_to_ticks_floor32(param));
+	printf("k_ms_to_ticks_floor64(%d) = %llu\r\n", param, k_ms_to_ticks_floor64(param));
+	printf("k_ms_to_ticks_near32(%d)  = %u\r\n", param,   k_ms_to_ticks_near32(param));
+	printf("k_ms_to_ticks_near64(%d)  = %llu\r\n", param, k_ms_to_ticks_near64(param));
+	printf("k_ms_to_ticks_ceil32(%d)  = %u\r\n", param,   k_ms_to_ticks_floor32(param));
+	printf("k_ms_to_ticks_ceil64(%d)  = %llu\r\n", param, k_ms_to_ticks_ceil64(param));
+
+	printf("k_us_to_cyc_floor32(%d) = %u\r\n", param,   k_us_to_cyc_floor32(param));
+	printf("k_us_to_cyc_floor64(%d) = %llu\r\n", param, k_us_to_cyc_floor64(param));
+	printf("k_us_to_cyc_near32(%d)  = %u\r\n", param,   k_us_to_cyc_near32(param));
+	printf("k_us_to_cyc_near64(%d)  = %llu\r\n", param, k_us_to_cyc_near64(param));
+	printf("k_us_to_cyc_ceil32(%d)  = %u\r\n", param,   k_us_to_cyc_floor32(param));
+	printf("k_us_to_cyc_ceil64(%d)  = %llu\r\n", param, k_us_to_cyc_ceil64(param));
+
+	printf("k_us_to_ticks_floor32(%d) = %u\r\n", param,   k_us_to_ticks_floor32(param));
+	printf("k_us_to_ticks_floor64(%d) = %llu\r\n", param, k_us_to_ticks_floor64(param));
+	printf("k_us_to_ticks_near32(%d)  = %u\r\n", param,   k_us_to_ticks_near32(param));
+	printf("k_us_to_ticks_near64(%d)  = %llu\r\n", param, k_us_to_ticks_near64(param));
+	printf("k_us_to_ticks_ceil32(%d)  = %u\r\n", param,   k_us_to_ticks_floor32(param));
+	printf("k_us_to_ticks_ceil64(%d)  = %llu\r\n", param, k_us_to_ticks_ceil64(param));
+
+	printf("k_ns_to_cyc_floor32(%d) = %u\r\n", param,   k_ns_to_cyc_floor32(param));
+	printf("k_ns_to_cyc_floor64(%d) = %llu\r\n", param, k_ns_to_cyc_floor64(param));
+	printf("k_ns_to_cyc_near32(%d)  = %u\r\n", param,   k_ns_to_cyc_near32(param));
+	printf("k_ns_to_cyc_near64(%d)  = %llu\r\n", param, k_ns_to_cyc_near64(param));
+	printf("k_ns_to_cyc_ceil32(%d)  = %u\r\n", param,   k_ns_to_cyc_floor32(param));
+	printf("k_ns_to_cyc_ceil64(%d)  = %llu\r\n", param, k_ns_to_cyc_ceil64(param));
+
+	printf("k_ns_to_ticks_floor32(%d) = %u\r\n", param,   k_ns_to_ticks_floor32(param));
+	printf("k_ns_to_ticks_floor64(%d) = %llu\r\n", param, k_ns_to_ticks_floor64(param));
+	printf("k_ns_to_ticks_near32(%d)  = %u\r\n", param,   k_ns_to_ticks_near32(param));
+	printf("k_ns_to_ticks_near64(%d)  = %llu\r\n", param, k_ns_to_ticks_near64(param));
+	printf("k_ns_to_ticks_ceil32(%d)  = %u\r\n", param,   k_ns_to_ticks_floor32(param));
+	printf("k_ns_to_ticks_ceil64(%d)  = %llu\r\n", param, k_ns_to_ticks_ceil64(param));
+
+	printf("k_cyc_to_ms_floor32(%d) = %u\r\n", param,   k_cyc_to_ms_floor32(param));
+	printf("k_cyc_to_ms_floor64(%d) = %llu\r\n", param, k_cyc_to_ms_floor64(param));
+	printf("k_cyc_to_ms_near32(%d)  = %u\r\n", param,   k_cyc_to_ms_near32(param));
+	printf("k_cyc_to_ms_near64(%d)  = %llu\r\n", param, k_cyc_to_ms_near64(param));
+	printf("k_cyc_to_ms_ceil32(%d)  = %u\r\n", param,   k_cyc_to_ms_ceil32(param));
+	printf("k_cyc_to_ms_ceil64(%d)  = %llu\r\n", param, k_cyc_to_ms_ceil64(param));
+
+	printf("k_cyc_to_us_floor32(%d) = %u\r\n", param,   k_cyc_to_us_floor32(param));
+	printf("k_cyc_to_us_floor64(%d) = %llu\r\n", param, k_cyc_to_us_floor64(param));
+	printf("k_cyc_to_us_near32(%d)  = %u\r\n", param,   k_cyc_to_us_near32(param));
+	printf("k_cyc_to_us_near64(%d)  = %llu\r\n", param, k_cyc_to_us_near64(param));
+	printf("k_cyc_to_us_ceil32(%d)  = %u\r\n", param,   k_cyc_to_us_ceil32(param));
+	printf("k_cyc_to_us_ceil64(%d)  = %llu\r\n", param, k_cyc_to_us_ceil64(param));
+
+	printf("k_cyc_to_ns_floor32(%d) = %u\r\n", param,   k_cyc_to_ns_floor32(param));
+	printf("k_cyc_to_ns_floor64(%d) = %llu\r\n", param, k_cyc_to_ns_floor64(param));
+	printf("k_cyc_to_ns_near32(%d)  = %u\r\n", param,   k_cyc_to_ns_near32(param));
+	printf("k_cyc_to_ns_near64(%d)  = %llu\r\n", param, k_cyc_to_ns_near64(param));
+	printf("k_cyc_to_ns_ceil32(%d)  = %u\r\n", param,   k_cyc_to_ns_ceil32(param));
+	printf("k_cyc_to_ns_ceil64(%d)  = %llu\r\n", param, k_cyc_to_ns_ceil64(param));
+
+	printf("k_cyc_to_ticks_floor32(%d) = %u\r\n", param,   k_cyc_to_ticks_floor32(param));
+	printf("k_cyc_to_ticks_floor64(%d) = %llu\r\n", param, k_cyc_to_ticks_floor64(param));
+	printf("k_cyc_to_ticks_near32(%d)  = %u\r\n", param,   k_cyc_to_ticks_near32(param));
+	printf("k_cyc_to_ticks_near64(%d)  = %llu\r\n", param, k_cyc_to_ticks_near64(param));
+	printf("k_cyc_to_ticks_ceil32(%d)  = %u\r\n", param,   k_cyc_to_ticks_ceil32(param));
+	printf("k_cyc_to_ticks_ceil64(%d)  = %llu\r\n", param, k_cyc_to_ticks_ceil64(param));
+
+	printf("k_ticks_to_ms_floor32(%d) = %u\r\n", param,   k_ticks_to_ms_floor32(param));
+	printf("k_ticks_to_ms_floor64(%d) = %llu\r\n", param, k_ticks_to_ms_floor64(param));
+	printf("k_ticks_to_ms_near32(%d)  = %u\r\n", param,   k_ticks_to_ms_near32(param));
+	printf("k_ticks_to_ms_near64(%d)  = %llu\r\n", param, k_ticks_to_ms_near64(param));
+	printf("k_ticks_to_ms_ceil32(%d)  = %u\r\n", param,   k_ticks_to_ms_ceil32(param));
+	printf("k_ticks_to_ms_ceil64(%d)  = %llu\r\n", param, k_ticks_to_ms_ceil64(param));
+
+	printf("k_ticks_to_us_floor32(%d) = %u\r\n", param,   k_ticks_to_us_floor32(param));
+	printf("k_ticks_to_us_floor64(%d) = %llu\r\n", param, k_ticks_to_us_floor64(param));
+	printf("k_ticks_to_us_near32(%d)  = %u\r\n", param,   k_ticks_to_us_near32(param));
+	printf("k_ticks_to_us_near64(%d)  = %llu\r\n", param, k_ticks_to_us_near64(param));
+	printf("k_ticks_to_us_ceil32(%d)  = %u\r\n", param,   k_ticks_to_us_ceil32(param));
+	printf("k_ticks_to_us_ceil64(%d)  = %llu\r\n", param, k_ticks_to_us_ceil64(param));
+
+	printf("k_ticks_to_ns_floor32(%d) = %u\r\n", param,   k_ticks_to_ns_floor32(param));
+	printf("k_ticks_to_ns_floor64(%d) = %llu\r\n", param, k_ticks_to_ns_floor64(param));
+	printf("k_ticks_to_ns_near32(%d)  = %u\r\n", param,   k_ticks_to_ns_near32(param));
+	printf("k_ticks_to_ns_near64(%d)  = %llu\r\n", param, k_ticks_to_ns_near64(param));
+	printf("k_ticks_to_ns_ceil32(%d)  = %u\r\n", param,   k_ticks_to_ns_ceil32(param));
+	printf("k_ticks_to_ns_ceil64(%d)  = %llu\r\n", param, k_ticks_to_ns_ceil64(param));
+
+	printf("k_ticks_to_cyc_floor32(%d) = %u\r\n", param,   k_ticks_to_cyc_floor32(param));
+	printf("k_ticks_to_cyc_floor64(%d) = %llu\r\n", param, k_ticks_to_cyc_floor64(param));
+	printf("k_ticks_to_cyc_near32(%d)  = %u\r\n", param,   k_ticks_to_cyc_near32(param));
+	printf("k_ticks_to_cyc_near64(%d)  = %llu\r\n", param, k_ticks_to_cyc_near64(param));
+	printf("k_ticks_to_cyc_ceil32(%d)  = %u\r\n", param,   k_ticks_to_cyc_ceil32(param));
+	printf("k_ticks_to_cyc_ceil64(%d)  = %llu\r\n", param, k_ticks_to_cyc_ceil64(param));
+
 	return 0;
 }
